66a17d5cf59e2383c3424e426d65b4bc03583212
Chris Dennis <chrisdennis0913@gmail.com>
2012-04-21 18:16:18 -0400

    with documentation

--- src/inventory/MakeItems.java
+++ src/inventory/MakeItems.java
@@ -3,12 +3,15 @@ package inventory;
 import java.util.ArrayList;
 import app.RPGame;
 
-
-/*
+/**
  * Items are constructed in the form 
- * (RPGgame, "ItemName, GifName, Category, forSale, price")
- * where forSale is a boolean and price is an int
- * Be sure to separate by commas and do no use within the Item's Name 
+ * (RPGame, "ItemName, GifName, Category")
+ * Be sure to separate by commas and do no use within the Item's Name
+ * Subclasses of Item have additional features in the toParse String
+ * Any time you need to make an Item, a MakeItems class should
+ * be used and passed the game.
+ * 
+ * @author Chris Dennis
  */
 public class MakeItems
 {

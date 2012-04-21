9902fb9457124069da9458aecc3fe863df703e21
Rachel Harris <rah35@duke.edu>
2012-04-21 16:51:02 -0400

    Incorporated dialogue structure into framework

---
+++ src/dialogue/SimpleDialogue.java
@@ -0,0 +1,31 @@
+package dialogue;
+
+public class SimpleDialogue extends AbstractDialogue{
+	
+	private String[] script;
+	private int index;
+
+	public SimpleDialogue(String url){
+		script = readFile(url);
+		index = 0;
+	}
+	public void goToNextLine(DialogueObject choice) {
+		if (!isDone())
+			index++;
+	}
+	
+	public String getCurrentLine(){
+		return script[index];
+	}
+
+	@Override
+	public boolean isDone() {
+		return index == script.length-1;
+	}
+	
+	public class SimpleDialogueObject extends DialogueObject{
+		
+		public SimpleDialogueObject(){};
+	}
+
+}

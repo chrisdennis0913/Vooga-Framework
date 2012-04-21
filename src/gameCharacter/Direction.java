b002f8c9f2dacaa46b07e3d92ba620dfe30ceb8f
kklimuk <kklimuk@gmail.com>
2012-04-21 17:31:19 -0400

    updated game character with directional functionality

--- src/gameCharacter/Direction.java
+++ src/gameCharacter/Direction.java
@@ -3,29 +3,32 @@ package gameCharacter;
 import java.awt.image.BufferedImage;
 
 public class Direction {
-	private BufferedImage[] images;
 	private GameCharacter character;
+	private final BufferedImage[] images;
 	private final int direction;
-	
-	public Direction (GameCharacter character, int direction, BufferedImage[] images) {
+	private final int delay;
+
+	public Direction(GameCharacter character, BufferedImage[] images,
+			int direction, int delay) {
 		this.images = images;
 		this.character = character;
 		this.direction = direction;
+		this.delay = delay;
 	}
-	
+
 	public int getDirection() {
 		return direction;
 	}
-	
+
 	public BufferedImage[] getImages() {
 		return images;
 	}
-	
+
 	public int frameCount() {
 		return images.length;
 	}
-	
-	public void changeCharacter (boolean animate, int delay) {
+
+	public void changeCharacter(boolean animate) {
 		if (animate) {
 			character.setImages(getImages());
 			if (frameCount() == 1) {
@@ -39,5 +42,17 @@ public class Direction {
 			}
 		}
 	}
+	
+	public String toString() {
+		if (direction == GameCharacter.DIR_DOWN)
+			return "DIR_DOWN";
+		else if (direction == GameCharacter.DIR_UP)
+			return "DIR_UP";
+		else if (direction == GameCharacter.DIR_LEFT)
+			return "DIR_LEFT";
+		else if (direction == GameCharacter.DIR_RIGHT)
+			return "DIR_RIGHT";
+		else
+			return "INVALID DIRECTION";
+	}
 }
-

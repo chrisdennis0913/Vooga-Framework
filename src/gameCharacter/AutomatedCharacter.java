b6244366debca91c04165881845df4b2e04c0597
James Hong <james.hong@duke.edu>
2012-04-22 10:43:08 -0400

    Modifications to AutomatedCharacter

--- src/gameCharacter/AutomatedCharacter.java
+++ src/gameCharacter/AutomatedCharacter.java
@@ -2,26 +2,38 @@ package gameCharacter;
 
 import dialogue.AbstractDialogue;
 
+/**
+ * Character that is non-playable and has movements
+ * controlled by an AI algorithm. Can have dialogue
+ * and alive states.
+ * @author jameshong
+ *
+ */
 public abstract class AutomatedCharacter extends GameCharacter{
 
 	//--- CONTROL FIELDS --------------------------------------
-	private AbstractMovementAI movAI;
+	private MotionController movAI;
 	private AbstractDialogue dialogue;
 	
 	//--- STATE FIELDS ----------------------------------------
 	private boolean alive;
 	private boolean canDie;
 	
-	public abstract void setAlive(boolean alive);
-	public abstract boolean isAlive();
-	
 	public AutomatedCharacter(){
 		super();
+	}
+
+	public void setAlive(boolean alive){
+		this.alive = alive;
+	}
+	
+	public boolean isAlive(){
+		return alive;
 	}
 	
 	public void update(long elapsedTime){
 		movAI.update(elapsedTime);
-		dialogue.update(elapsedTime);
+		dialogue.update(elapsedTime);	
 	}
 	
 }

53394a31610223e4fd7ca12c23ad53acc360f1d6
mross22 <ross.michael22@gmail.com>
2012-04-22 13:38:11 -0400

    adding JSON classes for enemy, player, and level

--- src/utils/JsonUtil.java
+++ src/utils/JsonUtil.java
@@ -18,7 +18,7 @@ public class JsonUtil {
 		for (String line : jsonPacked) {
 			jsonBuilder.append(line);
 		}
-
+		
 		return jsonBuilder.toString();
 	}
 	
@@ -40,6 +40,35 @@ public class JsonUtil {
 		public int[] right;
 	}
 
+	public class JSONLevel{
+		public String background;
+		public String nextLevel;
+		//public Player player;
+		//public Enemy[] enemies;
+		//public NPC[] npcs;		
+	}
+	 public class JSONPlayer{
+		 public int[] location;
+		 public String directions;
+		 public String actions;		 
+	 }
+	 
+	 public class JSONEnemy{
+		 public int[] location;
+		 public String directions;
+		 public String actions;		 
+	 }
+	 
+	 public class JSONNpc{
+		 public int[] location;
+	}
+	 
+	 public class JSONScenery{
+		 public int[] locations;
+		 public String imageURL;
+		 public int layer;
+	 }
+	 
 	public class JSONPlayerActions {
 		public JSONPlayerWalking walking;
 	}

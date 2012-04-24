2fd7f9cf2d4c296f809829321c98de984b5566ea
Rachel Harris <rah35@duke.edu>
2012-04-24 19:36:15 -0400

    created and partly incorporated scripted movements

--- src/ai/ScriptedMovementAI.java
+++ src/ai/ScriptedMovementAI.java
@@ -21,14 +21,23 @@ public class ScriptedMovementAI extends AbstractMovementAI{
 		super(game, character);
 		this.actions = actions;
 		actionsIndex = 0;
+		character.setVelocity(0.05);
+		character.setActiveDirection(actions[0][0]);
+		timer = new Timer(actions[0][1]);
 	}
 
 	@Override
 	public void update(long elapsedTime) {
-		if (actionsIndex == actions.length)
+		if (actionsIndex == actions.length){
 			character.setVelocity(0);
-		if (timer.action(elapsedTime)){
-			
+			character.stop();
+		}
+		else if (timer.action(elapsedTime)){
+			actionsIndex++;
+			if (actionsIndex != actions.length){
+				character.setActiveDirection(actions[actionsIndex][0]);
+				timer = new Timer(actions[actionsIndex][1]);
+			}
 		}
 	}
 

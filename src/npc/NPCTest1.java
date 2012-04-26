add7bf7a99a66c96bac795894f6feedc8a321a37
Rachel Harris <rah35@duke.edu>
2012-04-26 12:40:25 -0400

    minor corrections to filenames

--- src/npc/NPCTest1.java
+++ src/npc/NPCTest1.java
@@ -7,11 +7,16 @@ import app.RPGame;
 
 public class NPCTest1 extends NPC{
 
+	/**
+	 * Computer-generated serial ID number 
+	 */
+	private static final long serialVersionUID = 4483591744499315422L;
+
 	public NPCTest1(RPGame game, Location loc, String configURL) {
 		super(game, loc, configURL);
 		int[][] testArray= new int[][] {{1, 2100}, {2, 2000}};
 		movAI = new ScriptedMovementAI(game, this, testArray);
-		dialogue = new SimpleDialogue("rsc/npc1.txt");
+		dialogue = new SimpleDialogue("rsc/savedmaps/npc1.txt");
 	}
 
 }

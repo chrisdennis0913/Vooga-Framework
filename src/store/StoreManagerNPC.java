38a13a475e96b2195dec5a0f29e15ccd0aa210ef
Zahava <zma3@duke.edu>
2012-04-26 14:59:11 -0400

    Store manager

---
+++ src/store/StoreManagerNPC.java
@@ -0,0 +1,14 @@
+package store;
+
+import utils.Location;
+import app.RPGame;
+import npc.NPC;
+
+public class StoreManagerNPC extends NPC{
+
+	public StoreManagerNPC(RPGame game, Location loc, String configURL) {
+		super(game, loc, configURL);
+		// TODO Auto-generated constructor stub
+	}
+
+}

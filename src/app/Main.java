6469a072a5d1f9e06ca5f3f79853d80689098eb2
Chris Dennis <chrisdennis0913@gmail.com>
2012-04-27 06:30:08 -0400

    630am cleaning

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,14 +1,10 @@
 package app;
 
 import java.awt.Dimension;
-
 import utils.JsonUtil;
-
-
 import com.golden.gamedev.GameEngine;
 import com.golden.gamedev.GameLoader;
 import com.golden.gamedev.GameObject;
-import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 
 
--- src/inventory/Accessory.java
+++ src/inventory/Accessory.java
@@ -3,17 +3,11 @@ package inventory;
 public interface Accessory {
     /**
      * Interface for items that attach to the hero and contribute to his stats.
-     * Accessories are of the form
-     * "Name, gifName, accessory, statToChange, valueOfChange" for example
-     * "Charismatic Chainmail, chainmail, accessory, health, 8"
+     * Accessories require variables such as
+     * "Name, gifName, accessory, statToChange, valueOfChange"
      * 
      * @author Chris Dennis
      */
-//    private boolean showsOnScreen = true;
-//    private int statChange;
-//    private int relX;
-//    private int relY;
-//    private String statisticalCategory;
 
     void setStatCategory (String statistic);
 
